32d78fab7de6e1e6ee246c4c5a3a4bae608f71cb
P H <p.houska@cgv.tugraz.at>
2023-01-22 01:30:13 +0100

    added TCHAR-aware number-to-string conversion function * will use std::to_string or std::to_wstring depending on TCHAR type (char vs. wchar_t) * https://stackoverflow.com/questions/46840919/calling-stdto-string-or-stdto-wstring-based-on-given-typedef

--- src/stringUtils.h
+++ src/stringUtils.h
@@ -3,6 +3,8 @@
 
 #include <sstream>
 
+#include <tchar.h>
+
 namespace stringUtils {
     template < typename val_T >
     val_T min( const val_T& val1, const val_T& val2 ) {
@@ -29,6 +31,35 @@ namespace stringUtils {
         numStream >> num;
         printf( "after conversion: %f\n", num );
     }
+
+    // https://stackoverflow.com/questions/46840919/calling-stdto-string-or-stdto-wstring-based-on-given-typedef
+    template<class Char>
+    struct string_traits;
+    template<>
+    struct string_traits<char>{
+        using string=std::string;
+        template<class T>
+        static string convert_to_string(T&& t){
+            using std::to_string;
+            return to_string(std::forward<T>(t));
+        }
+    };
+    template<>
+    struct string_traits<wchar_t>{
+        using string=std::wstring;
+        template<class T>
+        static string convert_to_string(T&& t){
+            using std::to_wstring;
+            return to_wstring(std::forward<T>(t));
+        }
+    };
+
+    template <typename TChar = TCHAR, typename T>
+    std::basic_string<TChar>
+        ToString(const T& Arg) {
+        return string_traits<TChar>::convert_to_string(Arg);
+    }
+
 }
 
 #endif // _STRINGUTILS_H_EA89F308_240F_4AE0_97B5_AFE55000B453
